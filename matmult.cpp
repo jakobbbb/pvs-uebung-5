631e3374e962a62da5f14d963e95c48e5ee95016
neyustudies <science-cat@gmx.net>
2021-02-03 22:33:50 +0100

    Add task 2 and 3
    task 2 needs a fix.. not working by now

--- matmult.cpp
+++ matmult.cpp
@@ -30,7 +30,7 @@ const char* KernelSource = "#define DIM " MAT_SIZE_STR
 
 //------------------------------------------------------------------------
 // task 1: reduction of field access
- 
+/*  
 const char* KernelSource = "#define DIM " MAT_SIZE_STR
                            "\n"
                            "__kernel void mult(__global float *A,"
@@ -48,27 +48,52 @@ const char* KernelSource = "#define DIM " MAT_SIZE_STR
                            "   }"
                            "}";
  
-
+ */
 
 
 //------------------------------------------------------------------------
 // task 2: Loop swapping
-/*
+/* 
 const char* KernelSource = "#define DIM " MAT_SIZE_STR
                            "\n"
                            "__kernel void mult(__global float *A,"
                            "                   __global float *B,"
                            "                   __global float *C) {"
-                           "   float tmp = .0f;"
+                           "   float tmp = 0.0;"
                            "   int i, j, k;"
                            "   j = get_global_id(0);"
                            "   i = get_gloabl_id(1);"
-                           "   for (k = 0; k < DIM; ++k)"
-                           "         tmp += A[i*DIM+k] * B[k*DIM+j];"
+                           "   for (k = 0; k < DIM; ++k) {"
+                           "       tmp += A[i*DIM+k] * B[k*DIM+j];"
+                           "   }"
                            "   C[i*DIM+j] = tmp;"
                            "}";
-
  */
+
+
+//------------------------------------------------------------------------
+// task 3: Memory optimization
+
+const char* KernelSource = "#define DIM " MAT_SIZE_STR
+                           "\n"
+                           "__kernel void mult(__global float *A,"
+                           "                   __global float *B,"
+                           "                   __global float *C) {"
+                           "   int i, j, k;"
+                           "   i = get_global_id(0);"
+                           "   float P[DIM];"
+                           "   for (k = 0; k < DIM; ++k) {"
+                           "       P[k] = A[i*DIM+k];"
+                           "   }"
+                           "   for (j = 0; j < DIM; ++j) {"
+                           "       float tmp = .0f;"
+                           "       for (k = 0; k < DIM; ++k) {"
+                           "           tmp += P[k] * B[k*DIM+j];"
+                           "       }"
+                           "       C[i*DIM+j] = tmp;"
+                           "   }"
+                           "}";
+ 
 
 /** **/
 int main(void) {
